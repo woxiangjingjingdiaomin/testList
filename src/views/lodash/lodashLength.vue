f6ec88f8dcd3a7cb975043a06f062ace239b2ca0
xzliang <yeyuyixiao@163.com>
2021-02-05 14:43:34 +0800

    lodash学习

--- src/views/lodash/lodashLength.vue
+++ src/views/lodash/lodashLength.vue
@@ -7,10 +7,31 @@ import _ from "lodash";
 export default {
   methods: {
     getTestList() {
-      //   console.log(_.compact([0, 2, false, 4, "", 5]));
-      var arr = [1];
-      var other = _.concat(arr, 3, [4]);
+      let objects = [
+        {
+          x: 1,
+          y: 2,
+        },
+        {
+          x: 2,
+          y: 1,
+        },
+        {
+          x: 3,
+          y: 4,
+        },
+      ];
+
+      let other = _.differenceWith(objects, [{ x: 1, y: 2 }], _.isEqual);
       console.log(other);
+
+      //   console.log(_.compact([0, 2, false, 4, "", 5]));
+      // var arr = [1];
+      // var other = _.concat(arr, [3, 4]);
+      // console.log(other);
+      // let arr = [3, 2, 1];
+      // let other = _.difference(arr, [3, 1]);
+      // console.log(other);
     },
   },
   mounted() {
